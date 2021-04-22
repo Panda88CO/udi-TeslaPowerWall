868556b6fe93c68d923172e1e523cf451ec67c3a
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-04-22 14:59:40 -0700

    tst

--- TeslaTest.py
+++ TeslaTest.py
@@ -1,57 +1,7 @@
 
 
-from api import API
-'''
-from const import (
-    DEFAULT_KW_ROUND_PERSICION,
-    SUPPORTED_OPERATION_MODES,
-    DeviceType,
-    GridState,
-    GridStatus,
-    LineStatus,
-    MeterType,
-    OperationMode,
-    Roles,
-    SyncType,
-    User,
-)
-from error import (
-    AccessDeniedError,
-    APIError,
-    MissingAttributeError,
-    PowerwallError,
-    PowerwallUnreachableError,
-)
-from helpers import assert_attribute, convert_to_kw
-from responses import (
-    LoginResponse,
-    Meter,
-    MetersAggregates,
-    PowerwallStatus,
-    SiteInfo,
-    SiteMaster,
-    Solar,
-)
-from powerwall import Powerwall
+from tesla_powerwall import Powerwall
 
-'''
-import requests
-import json
-
-powerwallStr = "https://192.168.1.151/api/"
-login = "login/Basic"
-
-password='coe123COE',
-email='christian.olgaard@gmail.com',
-usertype = 'customer'
-
-testPW = API( "192.168.1.151", 10, None, False, True)
-response = testPW.login(usertype, email, password, False)
-print(testPW.is_authenticated())
-
-
-#resp = requests.put(loginstr, json=credentials, verify=False)
-'''
 PowerWall = Powerwall("192.168.1.151")
 PowerWall.login("coe123COE", "christian.olgaard@gmail.com")
 print(PowerWall.is_authenticated())
@@ -61,4 +11,3 @@ meters = PowerWall.get_meters()
 
 
 PowerWall.logout()
-'''

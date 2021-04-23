143dec102bb0b8dfc48f643a2b470bdcde04c386
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-04-22 19:01:19 -0700

    t

--- TeslaTest.py
+++ TeslaTest.py
@@ -12,5 +12,49 @@ meters = PowerWall.get_meters()
 
 
 
-print(meters)
-#PowerWall.logout()
+
+
+#print(PowerWall.run())
+#print(PowerWall.stop())
+#print(PowerWall.run())
+
+print(PowerWall.get_charge())
+
+print(PowerWall.get_sitemaster())
+
+print(PowerWall.get_meters())
+
+print(PowerWall.get_grid_status())
+print(PowerWall.is_grid_services_active())
+
+print(PowerWall.get_site_info())
+
+#print(PowerWall.set_site_name(, site_name: str))
+
+print(PowerWall.get_status())
+
+print(PowerWall.get_device_type())
+print(PowerWall.get_serial_numbers())
+print(PowerWall.get_operation_mode())
+
+print(PowerWall.get_backup_reserve_percentage())
+print(PowerWall.get_solars())
+
+print(PowerWall.get_vin())
+
+print(PowerWall.get_version())
+
+print(PowerWall.detect_and_pin_version())
+
+
+#print(PowerWall.pin_version(, vers: Union[str, version.Version]))
+
+
+print(PowerWall.get_pinned_version())
+
+
+print(PowerWall.get_api())
+
+
+print(PowerWall.close())
+#print(PowerWall.logout())

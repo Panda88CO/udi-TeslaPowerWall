3f881fd512a1e6b9439dd5c5e0e66a89633695cf
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-05-04 19:00:02 -0700

    Update TeslaPWController.py

--- TeslaPWController.py
+++ TeslaPWController.py
@@ -80,7 +80,7 @@ class TeslaPWController(polyinterface.Controller):
             for key in self.ISYparams:
                 info = self.ISYparams[key]
                 if info != {}:
-                    val = self.TPW.getISYvalue(key, self.id)
+                    value = self.TPW.getISYvalue(key, self.id)
                     self.drivers.append({'driver':key, 'value':value, 'uom':info['uom'] })
                     self.setDriver(key, value, report = True, force = True)   
         self.nodeDefineDone = True

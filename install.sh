e1af9af9ba7b2f89e5eaac628b5c34122b40f84b
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-09-02 13:31:12 -0700

    Polisy Install

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ mkdir -p profile/editor
 #needed for Polisy operation
 sudo pkg install py38-pillow
 # only needed for PRi operation
-sudo apt-get install libxml2-dev libxslt-dev python-dev
+#sudo apt-get install libxml2-dev libxslt-dev python-dev
 
 pip install -r requirements.txt --user

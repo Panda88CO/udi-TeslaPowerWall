5d93189d6f56dcfbb3c34a567dc08b421bda4197
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-09-02 10:30:29 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt --user
+
 
 mkdir -p profile 
 mkdir -p profile/nodedef
 mkdir -p profile/nls
 mkdir -p profile/editor
+
+sudo pkg install py38-pillow
+sudo apt-get install libxml2-dev libxslt-dev python-dev
+
+pip install -r requirements.txt --user

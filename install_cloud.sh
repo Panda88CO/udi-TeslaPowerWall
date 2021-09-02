754e6056bbce4c9edfa530a2c92f56af1fa7c960
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-09-02 15:20:22 -0700

    New Release candidate

--- install_cloud.sh
+++ install_cloud.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-pip3 install -r requirements_cloud.txt --user
+
 
 mkdir -p profile 
 mkdir -p profile/nodedef
 mkdir -p profile/nls
 mkdir -p profile/editor
+
+#needed for Polisy operation
+#sudo pkg install py38-pillow
+# only needed for PRi operation
+#sudo apt-get install libxml2-dev libxslt-dev python-dev
+
+pip3 install -r requirements_cloud.txt --user

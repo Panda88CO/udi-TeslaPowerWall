04197691ae876e70892199ceebe7d77273df54d5
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-09-03 15:21:25 -0700

    t

--- install.sh
+++ install.sh
@@ -11,4 +11,16 @@ sudo pkg install py38-pillow
 # only needed for PRi operation
 sudo apt-get install libxml2-dev libxslt-dev python-dev
 
-pip install -r requirements.txt --user
+pip install -U pip
+pip3 install -U pip3
+
+#pip install -U pip
+pip3 install -r tesla_powerwall --user
+pip3 install -r requests_oauth2 --user
+pip3 install -r pgc_interface --user
+
+pip3 install -r svglib --user
+pip3 install -r reportlab --user
+pip3 install -r requests --user
+
+#pip install -r requirements.txt --user
--- install_cloud.sh
+++ install_cloud.sh
@@ -9,6 +9,18 @@ mkdir -p profile/editor
 #needed for Polisy operation
 #sudo pkg install py38-pillow
 # only needed for PRi operation
-#sudo apt-get install libxml2-dev libxslt-dev python-dev
 
-pip3 install -r requirements_cloud.txt --user
+pip install -U pip
+
+sudo apt-get install libxml2-dev libxslt-dev python-dev
+
+pip3 install -U pip3
+pip3 install -r tesla_powerwall --user
+pip3 install -r requests_oauth2 --user
+pip3 install -r pgc_interface --user
+
+pip3 install -r svglib --user
+pip3 install -r reportlab --user
+pip3 install -r requests --user
+
+#pip3 install -r requirements_cloud.txt --user

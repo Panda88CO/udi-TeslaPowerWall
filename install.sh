a781fd76b9ca697b47021f48904dede06a983548
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2021-09-03 17:46:42 -0700

    test

--- install.sh
+++ install.sh
@@ -8,19 +8,8 @@ mkdir -p profile/editor
 
 #needed for Polisy operation
 sudo pkg install py38-pillow
-# only needed for PRi operation
+#needed for PRi operation
 sudo apt-get install libxml2-dev libxslt-dev python-dev
 
-pip install -U pip
-pip3 install -U pip3
-
-#pip install -U pip
-pip3 install -r tesla_powerwall --user
-pip3 install -r requests_oauth2 --user
-pip3 install -r pgc_interface --user
-
-pip3 install -r svglib --user
-pip3 install -r reportlab --user
-pip3 install -r requests --user
-
-#pip install -r requirements.txt --user
+pip install --upgrade pip
+pip install -r requirements.txt --user
